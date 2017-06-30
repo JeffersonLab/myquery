ef0ae011aee91592d8367c1901874e1f6de7bf84
slominskir <Ryan.Slominski@gmail.com>
2017-06-30 14:07:00 -0400

    Initial support for span query using jmyapi

---
+++ src/java/org/jlab/myGet/JmyapiSpanService.java
@@ -0,0 +1,56 @@
+package org.jlab.myGet;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import org.jlab.mya.Deployment;
+import org.jlab.mya.Metadata;
+import org.jlab.mya.event.FloatEvent;
+import org.jlab.mya.nexus.PooledNexus;
+import org.jlab.mya.params.IntervalQueryParams;
+import org.jlab.mya.service.IntervalService;
+import org.jlab.mya.stream.FloatEventStream;
+
+/**
+ *
+ * @author ryans
+ */
+public class JmyapiSpanService implements SpanService {
+
+    private static final PooledNexus NEXUS;
+
+    static {
+        try {
+            NEXUS = new PooledNexus(Deployment.ops);
+        } catch (NamingException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    @Override
+    public List<Record> getRecordList(String c, String b, String e, String l, String p, String m,
+            String M, String d, String f, String s) throws Exception {
+        List<Record> recordList = new ArrayList<>();
+        IntervalService service = new IntervalService(NEXUS); 
+        Metadata metadata = service.findMetadata(c);
+        Instant begin = LocalDateTime.parse(b).atZone(
+            ZoneId.systemDefault()).toInstant();
+        Instant end = LocalDateTime.parse(e).atZone(
+            ZoneId.systemDefault()).toInstant();
+        IntervalQueryParams params = new IntervalQueryParams(metadata, begin, end);
+        try (FloatEventStream stream = service.openFloatStream(params)) {
+            FloatEvent event;
+            while ((event = stream.read()) != null) {
+                System.out.println(event);
+            }
+        }
+
+        return recordList;
+    }
+
+}
