2b71b8b260e83b226dac10e855926c5a129a7a48
ryans <ryans@jlab.org>
2017-07-05 15:53:29 -0400

    Show code instead of value if not an UPDATE

--- src/java/org/jlab/myGet/JmyapiSpanService.java
+++ src/java/org/jlab/myGet/JmyapiSpanService.java
@@ -3,17 +3,13 @@ package org.jlab.myGet;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.ArrayList;
-import java.util.List;
 import javax.naming.NamingException;
 import org.jlab.mya.Deployment;
 import org.jlab.mya.EventStream;
 import org.jlab.mya.Metadata;
-import org.jlab.mya.event.FloatEvent;
 import org.jlab.mya.nexus.PooledNexus;
 import org.jlab.mya.params.IntervalQueryParams;
 import org.jlab.mya.service.IntervalService;
-import org.jlab.mya.stream.FloatEventStream;
 
 /**
  *
@@ -31,26 +27,6 @@ public class JmyapiSpanService {
         }
     }
 
-    public List<Record> getRecordList(String c, String b, String e, String l, String p, String m,
-            String M, String d, String f, String s) throws Exception {
-        List<Record> recordList = new ArrayList<>();
-        IntervalService service = new IntervalService(NEXUS); 
-        Metadata metadata = service.findMetadata(c);
-        Instant begin = LocalDateTime.parse(b).atZone(
-            ZoneId.systemDefault()).toInstant();
-        Instant end = LocalDateTime.parse(e).atZone(
-            ZoneId.systemDefault()).toInstant();
-        IntervalQueryParams params = new IntervalQueryParams(metadata, begin, end);
-        try (FloatEventStream stream = service.openFloatStream(params)) {
-            FloatEvent event;
-            while ((event = stream.read()) != null) {
-                System.out.println(event);
-            }
-        }
-
-        return recordList;
-    }
-
     public EventStream openEventStream(String c, String b, String e, String l, String p, String m,
             String M, String d, String f, String s) throws Exception {
         IntervalService service = new IntervalService(NEXUS); 
@@ -60,8 +36,6 @@ public class JmyapiSpanService {
         Instant end = LocalDateTime.parse(e).atZone(
             ZoneId.systemDefault()).toInstant();
         IntervalQueryParams params = new IntervalQueryParams(metadata, begin, end);
-        final FloatEventStream stream = service.openFloatStream(params);
-
-        return stream;      
+        return service.openFloatStream(params);  
     }
 }
