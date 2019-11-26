78b754c5c1da28f6dfd3fe7433d9dd2c19cdf057
ryans <ryans@jlab.org>
2019-11-26 09:07:00 -0500

    Use jmyapi 5.0.0

--- src/main/java/org/jlab/myquery/PointWebService.java
+++ src/main/java/org/jlab/myquery/PointWebService.java
@@ -19,7 +19,7 @@ public class PointWebService extends QueryWebService {
     private DataNexus nexus;
 
     public PointWebService(String deployment) {
-        DataNexus nexus = getNexus(deployment);
+        nexus = getNexus(deployment);
     }
     
     public Metadata findMetadata(String c) throws SQLException {
@@ -28,6 +28,7 @@ public class PointWebService extends QueryWebService {
 
     @SuppressWarnings("unchecked")
     public Event findEvent(Metadata metadata, boolean updatesOnly, Instant t, boolean lessThan, boolean orEqual, boolean enumsAsStrings) throws SQLException {
+
         Event event = nexus.findEvent(metadata, t, lessThan, orEqual, updatesOnly);
         
         if(enumsAsStrings && metadata.getMyaType() == MyaDataType.DBR_ENUM) {
