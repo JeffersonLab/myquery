157b81b060263dd6a116b6065ff569b5aaa31c6b
slominskir <ryan.slominski@gmail.com>
2023-09-26 14:17:49 -0400

    mystats tests

--- src/integration/java/org/jlab/myquery/MyStatsQueryTest.java
+++ src/integration/java/org/jlab/myquery/MyStatsQueryTest.java
@@ -41,8 +41,8 @@ public class MyStatsQueryTest {
                       "begin": "2019-08-12 00:00:00.000",
                       "eventCount": 335,
                       "updateCount": 334,
-                      "duration": 719.128,
-                      "integration": 68755.1,
+                      "duration": 719.1279857158660888671875,
+                      "integration": 68755.144563262627343647181987762451171875,
                       "max": 96.8135,
                       "mean": 95.6091,
                       "min": 94.4322,
@@ -53,8 +53,8 @@ public class MyStatsQueryTest {
                       "begin": "2019-08-12 00:12:00.000",
                       "eventCount": 369,
                       "updateCount": 368,
-                      "duration": 719.119,
-                      "integration": 68944.1,
+                      "duration": 719.1194341182708740234375,
+                      "integration": 68944.138235634469310753047466278076171875,
                       "max": 96.8513,
                       "mean": 95.8730,
                       "min": 94.9427,
@@ -65,8 +65,8 @@ public class MyStatsQueryTest {
                       "begin": "2019-08-12 00:24:00.000",
                       "eventCount": 343,
                       "updateCount": 342,
-                      "duration": 718.113,
-                      "integration": 68751.7,
+                      "duration": 718.11330127716064453125,
+                      "integration": 68751.66884353742352686822414398193359375,
                       "max": 96.8915,
                       "mean": 95.7393,
                       "min": 95.0116,
@@ -77,8 +77,8 @@ public class MyStatsQueryTest {
                       "begin": "2019-08-12 00:36:00.000",
                       "eventCount": 317,
                       "updateCount": 316,
-                      "duration": 718.074,
-                      "integration": 65907.7,
+                      "duration": 718.073926448822021484375,
+                      "integration": 65907.665449329026159830391407012939453125,
                       "max": 96.9524,
                       "mean": 91.7840,
                       "min": 0,
@@ -89,8 +89,8 @@ public class MyStatsQueryTest {
                       "begin": "2019-08-12 00:48:00.000",
                       "eventCount": 352,
                       "updateCount": 351,
-                      "duration": 714.116,
-                      "integration": 68422.2,
+                      "duration": 714.1164848804473876953125,
+                      "integration": 68422.188102417494519613683223724365234375,
                       "max": 96.9037,
                       "mean": 95.8138,
                       "min": 94.8486,
@@ -212,7 +212,7 @@ public class MyStatsQueryTest {
                      "eventCount": 32963,
                      "updateCount": 32962,
                      "duration": 305970,
-                     "integration": 2.72142E+7,
+                     "integration": 27214184.8855658471584320068359375,
                      "max": 103.997,
                      "mean": 88.9440,
                      "min": 0,
@@ -224,7 +224,7 @@ public class MyStatsQueryTest {
                      "eventCount": 2,
                      "updateCount": 1,
                      "duration": 305970,
-                     "integration": 2.92824E+7,
+                     "integration": 29282429.886932373046875,
                      "max": 95.7036,
                      "mean": 95.7036,
                      "min": 95.7036,
@@ -259,7 +259,7 @@ public class MyStatsQueryTest {
 
         assertEquals(200, response.statusCode());
 
-        String exp = "1234test({\"channels\":{\"channel1\":{\"metadata\":{\"name\":\"channel1\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"mya\",\"ioc\":null,\"active\":true},\"data\":[{\"begin\":\"2019-08-12 00:00:00.000\",\"eventCount\":1716,\"updateCount\":1715,\"duration\":3594.42,\"integration\":341342,\"max\":96.9524,\"mean\":94.9644,\"min\":0,\"rms\":95.2675,\"stdev\":7.59226}],\"returnCount\":1},\"channel4\":{\"metadata\":{\"name\":\"channel4\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"mya\",\"ioc\":null,\"active\":true},\"data\":[{\"begin\":\"2019-08-12 00:00:00.000\",\"eventCount\":0,\"updateCount\":0,\"duration\":null,\"integration\":null,\"max\":null,\"mean\":null,\"min\":null,\"rms\":null,\"stdev\":null}],\"returnCount\":1}}});";
+        String exp = "1234test({\"channels\":{\"channel1\":{\"metadata\":{\"name\":\"channel1\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"mya\",\"ioc\":null,\"active\":true},\"data\":[{\"begin\":\"2019-08-12 00:00:00.000\",\"eventCount\":1716,\"updateCount\":1715,\"duration\":3594.42103290557861328125,\"integration\":341342.2010727164451964199542999267578125,\"max\":96.9524,\"mean\":94.9644,\"min\":0,\"rms\":95.2675,\"stdev\":7.59226}],\"returnCount\":1},\"channel4\":{\"metadata\":{\"name\":\"channel4\",\"datatype\":\"DBR_DOUBLE\",\"datasize\":1,\"datahost\":\"mya\",\"ioc\":null,\"active\":true},\"data\":[{\"begin\":\"2019-08-12 00:00:00.000\",\"eventCount\":0,\"updateCount\":0,\"duration\":null,\"integration\":null,\"max\":null,\"mean\":null,\"min\":null,\"rms\":null,\"stdev\":null}],\"returnCount\":1}}});";
 
         String result;
         try (BufferedReader reader = new BufferedReader(new StringReader(response.body()))) {
